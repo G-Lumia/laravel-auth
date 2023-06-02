a4bef2a4bdfb1ca579487bf3a5bed2e692e91f70
G-Lumia <giorgiolumia@gmail.com>
2023-06-02 15:17:02 +0200

    fixed home page style

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="root-color">
+    <div>
+        <div class="container d-flex align-items-center justify-content-between py-4">
+            <div><button class="text-uppercase fw-semibold btn btn-yellow">Write an email </button></div>
+            <div class="d-flex align-items-center text-white">
+                <h5 class="text-uppercase my-0">Contact me </h5>
+                <div class="ms-4 fs-4 links">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-youtube"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-tiktok"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
