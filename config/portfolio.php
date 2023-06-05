37bad049b8f2ce64edaf86a393419a6d38027fc0
G-Lumia <giorgiolumia@gmail.com>
2023-06-05 13:20:55 +0200

    data for seeder

---
+++ config/portfolio.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'projects' => [
+        [
+            "image" => "https://www.lanternaweb.it/wp-content/uploads/2022/05/netflix-logo-2.jpg",
+            "name" => "Vite-Boolflix",
+            "link" => "https://github.com/G-Lumia/vite-boolflix"
+        ],
+        [
+            "image" => "https://m.media-amazon.com/images/I/51eb2eOr2pL._AC_UF1000,1000_QL80_.jpg",
+            "name" => "Vite-Yu-Gi-Oh",
+            "link" => "https://github.com/G-Lumia/vite-yu-gi-oh"
+        ],
+        [
+            "image" => "https://static.posters.cz/image/750/stampa-su-tela-dc-comics-heroes-together-i111090.jpg",
+            "name" => "Laravel-Comics",
+            "link" => "https://github.com/G-Lumia/laravel-comics"
+        ],
+        [
+            "image" => "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4F1hK?ver=f476",
+            "name" => "Game-Creator",
+            "link" => "https://github.com/Dan848/game-creator"
+        ]
+    ]
+];
